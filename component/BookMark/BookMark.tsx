66256ca649ad8f00ce4964cd59f597e7b4020611
seo337dc <sdc337dc@gmail.com>
2021-12-02 11:36:10 +0900

    add : 화면 왼쪽 네비게이션 레이아웃 (즐겨찾기, 로그인 컴포넌트)

---
+++ component/BookMark/BookMark.tsx
@@ -0,0 +1,5 @@
+const BookMark = () =>{
+  return <div></div>;
+};
+
+export default BookMark;
---
+++ component/Login/Login.tsx
@@ -0,0 +1,93 @@
+import styled from 'styled-components';
+
+const Login = () => {
+  return (
+    <LoginWrap>
+      <HeadWrap>
+        LOGIN
+      </HeadWrap>
+      <BodyWrap>
+        <BodyContainer>
+          <BodyContent>
+            <InputCotent>ID</InputCotent>
+            <input />
+          </BodyContent>
+          <BodyContent>
+            <InputCotent>PW</InputCotent>
+            <input />
+          </BodyContent>
+          <ButtonWrap>
+            <ButtonStyle>로그인</ButtonStyle>
+          </ButtonWrap>
+
+        </BodyContainer>
+      </BodyWrap>
+    </LoginWrap>
+  );
+};
+
+const LoginWrap = styled.div`
+  height: 400px;
+  border: 1px solid black;
+`;
+
+const HeadWrap = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #000;
+  color: #fff;
+  font-size: x-large;
+`;
+
+const BodyWrap = styled.div`
+  height: 350px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+`;
+
+const BodyContainer = styled.div``;
+
+const BodyContent = styled.div`
+  height: 30px;
+  margin: 10px 0;
+  display: flex;
+  gap: 5px;
+
+  input {
+    width: 150px;
+  }
+`;
+const InputCotent = styled.div`
+  width: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+
+  &:after {
+    content: ':';
+  }
+`;
+
+const ButtonWrap = styled.div`
+  height: 30px;
+  margin: 40px 0;
+  display: flex;
+  gap: 5px;
+`;
+
+const ButtonStyle = styled('button')`
+  width: 100%;
+  height: 40px;
+  border : 0;
+  background-color: #000;
+  color: #fff;
+  cursor: pointer;
+`;
+
+export default Login;
